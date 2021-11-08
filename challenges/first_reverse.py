51de4f70dcbb50cae3ec62599e16c7f6260a83e2
marcosranes <marcosranes@gmail.com>
2021-11-08 05:30:29 -0300

    added first_reverse.py function

---
+++ challenges/first_reverse.py
@@ -0,0 +1,29 @@
+"""
+    expected:
+    >>> first_reverse("python language")
+    'egaugnal nohtyp'
+"""
+
+"""
+First Reverse
+Have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order. For
+example: if the input string is "Hello World and Coders" then your program should return the string sredoC dna dlroW 
+olleH.
+
+Examples
+Input: "coderbyte"
+Output: etybredoc
+Input: "I Love Code"
+Output: edoC evoL I
+"""
+
+
+def first_reverse(param):
+    # code goes here
+    return param[::-1]
+
+
+# keep this function call here
+if __name__ == '__main__':
+    print(first_reverse("I Love Code"))
+    print(first_reverse("coderbyte"))
