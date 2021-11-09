6d658edc0dbded774b64ad717b1a226cd71cc5ca
marcosranes <marcosranes@gmail.com>
2021-11-09 19:45:59 -0300

    added range tests case between 1 and 18

--- challenges/first_factorial.py
+++ challenges/first_factorial.py
@@ -1,8 +1,7 @@
 """
     expected:
-    >>> for f in range(11):
+    >>> for f in range(1, 19):
     ...     f"{f}!  =>  {first_factorial(f)}"
-    '0!  =>  0'
     '1!  =>  1'
     '2!  =>  2'
     '3!  =>  6'
@@ -13,6 +12,14 @@
     '8!  =>  40320'
     '9!  =>  362880'
     '10!  =>  3628800'
+    '11!  =>  39916800'
+    '12!  =>  479001600'
+    '13!  =>  6227020800'
+    '14!  =>  87178291200'
+    '15!  =>  1307674368000'
+    '16!  =>  20922789888000'
+    '17!  =>  355687428096000'
+    '18!  =>  6402373705728000'
 """
 
 """
@@ -38,6 +45,5 @@ def first_factorial(num: int) -> int:
 
 # keep this function call here
 if __name__ == '__main__':
-    print(first_factorial(1))  # 1
     print(first_factorial(4))  # 24
     print(first_factorial(8))  # 40320
